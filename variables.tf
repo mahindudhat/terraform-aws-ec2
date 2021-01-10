eedcc795680361ea229aeceb5a22cee7f92c06de
mahindudhat <mayurdudhat55598@gmail.com>
2021-01-10 10:06:08 -0500

    Terraformfiles

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "region" {
+  default = "us-east-2"
+}
+
+variable "accesskey" {
+	default="" 
+}
+
+variable "secretkey" {
+	default=""
+}
+
+variable "amiID" {
+  default = "ami-0a91cd140a1fc148a"
+}
+
+variable "keyname" {
+	default="Nagios"
+}
+
+variable "vpcID" {
+	default="vpc-3677d55d"
+}
+
+
+
+
